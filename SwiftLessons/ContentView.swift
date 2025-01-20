b56de19ba15e618c9270b1154bba4451b64c390b
mylesgrxnt <grantmx@bc.edu>
2025-01-20 17:02:52 -0500

    Lesson 1.2

--- SwiftLessons/ContentView.swift
+++ SwiftLessons/ContentView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = "I am a Programmer"
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+            Button("Click Me!") {
+                message = "Awesome!"
+            }
         }
         .padding()
+        
     }
 }
 
