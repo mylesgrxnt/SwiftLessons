db923ced66e6375c14fec5ab932f898624cd71ef
mylesgrxnt <grantmx@bc.edu>
2025-01-20 16:59:26 -0500

    Initial Commit

---
+++ SwiftLessons/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftLessons
+//
+//  Created by Myles Grant on 1/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftLessons/SwiftLessonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLessonsApp.swift
+//  SwiftLessons
+//
+//  Created by Myles Grant on 1/20/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLessonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
